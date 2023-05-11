ad60b71bebb6cb38434f4013b634486754f6a438
Jp Sia <johnpaulsia4@gmail.com>
2023-05-11 21:28:59 +0800

    feat: add main-nav component

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,43 +1,33 @@
-import * as React from 'react'
-import Link from 'next/link'
+import NavLink from './nav-link'
 
-import { NavItem } from '@/types/nav'
-import { siteConfig } from '@/config/site'
-import { cn } from '@/lib/utils'
-import { Icons } from '@/components/icons'
-
-type MainNavProps = {
-  items?: NavItem[]
+type NavLink = {
+  href: string
+  title: string
 }
 
-export default function MainNav({ items }: MainNavProps) {
+const navlinks: NavLink[] = [
+  {
+    href: '#services',
+    title: 'Services',
+  },
+  {
+    href: '#about',
+    title: 'About Us',
+  },
+  {
+    href: '#contact-us',
+    title: 'Contact Us',
+  },
+]
+
+export default function MainNav() {
   return (
-    <div className='flex gap-6 md:gap-10'>
-      <Link href='/' className='hidden items-center space-x-2 md:flex'>
-        <Icons.logo className='h-6 w-6' />
-        <span className='hidden font-bold sm:inline-block'>
-          {siteConfig.name}
-        </span>
-      </Link>
-      {items?.length ? (
-        <nav className='hidden gap-6 md:flex'>
-          {items?.map(
-            (item, index) =>
-              item.href && (
-                <Link
-                  key={index}
-                  href={item.href}
-                  className={cn(
-                    'flex items-center text-lg font-semibold text-muted-foreground sm:text-sm',
-                    item.disabled && 'cursor-not-allowed opacity-80'
-                  )}
-                >
-                  {item.title}
-                </Link>
-              )
-          )}
-        </nav>
-      ) : null}
+    <div className='hidden md:flex md:gap-x-6'>
+      {navlinks.map(({ href, title }) => (
+        <NavLink href={href} key={href}>
+          {title}
+        </NavLink>
+      ))}
     </div>
   )
 }
