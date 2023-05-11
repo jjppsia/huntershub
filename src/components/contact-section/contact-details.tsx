c232d9a5eb7d45303400e0f4348f7631dc969f54
Jp Sia <johnpaulsia4@gmail.com>
2023-05-11 22:58:37 +0800

    refactor: re-organize components

--- src/components/contact-section/contact-details.tsx
+++ src/components/contact-section/contact-details.tsx
@@ -14,48 +14,38 @@ type ContactDetailsProps = {
 
 export default function ContactDetails({ contactItems }: ContactDetailsProps) {
   return (
-    <div className='relative px-6 lg:static lg:px-8'>
-      <div className='mx-auto max-w-xl lg:mx-0 lg:max-w-lg'>
-        <h2 className='text-3xl font-bold text-foreground'>Get in touch</h2>
-        <p className='mt-6 text-lg leading-8 text-foreground'>
-          Proin volutpat consequat porttitor cras nullam gravida at. Orci
-          molestie a eu arcu. Sed ut tincidunt integer elementum id sem. Arcu
-          sed malesuada et magna.
-        </p>
-        <dl className='mt-10 space-y-4 leading-7 text-slate-600'>
-          <div className='flex gap-x-4'>
+    <dl className='mt-10 space-y-4 leading-7 text-slate-600'>
+      <div className='flex gap-x-4'>
+        <dt className='flex-none'>
+          <span className='sr-only'>Address</span>
+          <Icons.building
+            className='h-7 w-6 text-slate-600'
+            aria-hidden='true'
+          />
+        </dt>
+        <dd>
+          123 Cant Find
+          <br />
+          Location, FR
+        </dd>
+      </div>
+      {contactItems.map(({ icon, text, href }) => {
+        const Icon = Icons[icon]
+
+        return (
+          <div key={text} className='flex gap-x-4'>
             <dt className='flex-none'>
-              <span className='sr-only'>Address</span>
-              <Icons.building
-                className='h-7 w-6 text-slate-600'
-                aria-hidden='true'
-              />
+              <span className='sr-only'>{icon}</span>
+              <Icon className='h-7 w-6 text-slate-600' aria-hidden='true' />
             </dt>
             <dd>
-              123 Cant Find
-              <br />
-              Location, FR
+              <Link className='hover:text-foreground' href={href}>
+                {text}
+              </Link>
             </dd>
           </div>
-          {contactItems.map(({ icon, text, href }) => {
-            const Icon = Icons[icon]
-
-            return (
-              <div key={text} className='flex gap-x-4'>
-                <dt className='flex-none'>
-                  <span className='sr-only'>{icon}</span>
-                  <Icon className='h-7 w-6 text-slate-600' aria-hidden='true' />
-                </dt>
-                <dd>
-                  <Link className='hover:text-foreground' href={href}>
-                    {text}
-                  </Link>
-                </dd>
-              </div>
-            )
-          })}
-        </dl>
-      </div>
-    </div>
+        )
+      })}
+    </dl>
   )
 }
--- src/components/contact-section/contact-section.tsx
+++ src/components/contact-section/contact-section.tsx
@@ -22,7 +22,19 @@ export default function ContactSection() {
       className='relative isolate bg-slate-50 py-20 text-primary-foreground sm:py-32'
     >
       <Container className='grid grid-cols-1 lg:grid-cols-2'>
-        <ContactDetails contactItems={contactItems} />
+        <div className='relative px-6 lg:static lg:px-8'>
+          <div className='mx-auto max-w-xl lg:mx-0 lg:max-w-lg'>
+            <h2 className='text-3xl font-bold tracking-tight text-foreground'>
+              Get in touch
+            </h2>
+            <p className='mt-6 text-lg leading-8 tracking-tight text-foreground'>
+              Proin volutpat consequat porttitor cras nullam gravida at. Orci
+              molestie a eu arcu. Sed ut tincidunt integer elementum id sem.
+              Arcu sed malesuada et magna.
+            </p>
+            <ContactDetails contactItems={contactItems} />
+          </div>
+        </div>
         <ContactForm />
       </Container>
     </section>
