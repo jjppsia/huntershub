7359f882bfa86d5cf905f525e38372476dba5391
Jp Sia <johnpaulsia4@gmail.com>
2023-05-12 20:02:12 +0800

    feat: add index file for footer section to have a cleaner import to root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import ContactSection from '@/components/contact-section'
-import FooterSection from '@/components/footer-section/footer-section'
+import FooterSection from '@/components/footer-section'
 import Hero from '@/components/hero-section/hero-section'
 import ServicesSection from '@/components/services-section'
 import ThreePillarsSection from '@/components/three-pillars-section'
